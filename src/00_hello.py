1755a92ea0be675b0f5293ca3b63e745d3192761
Matt Hagner <web@matthagner.com>
2020-02-12 17:24:51 -0600

    Complete 00_hello

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,8 @@
-# Print "Hello, world!" to your terminal
+""" Print "Hello, world!" to your terminal """
+
+
+def hello_world():
+    """ prints 'Hello, world!' to the terminal """
+    print("Hello, world!")
+
+hello_world()
