76905f823ca7ae4559560229c41e3e8e80a10ffd
Matt Hagner <web@matthagner.com>
2020-02-12 17:29:27 -0600

    Solve 01_bignum

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,10 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+
+def print_big():
+    """ prints a heckin big number """
+    print(2 ** 65536)
+
+print_big()
